0143a15292c24cfed2ce1df94000b0d2ea258008
Omar Wael <omarwael524@gmail.com>
2026-05-13 22:48:59 +0300

    fix: The disapernce of text when the search request is sent

--- jobs/views/joblist.py
+++ jobs/views/joblist.py
@@ -11,3 +11,14 @@ class job_list_view(ListView):
         if query:
             return Job.objects.filter(title__icontains=query)
         return Job.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        """Passes the search query and count back to the template."""
+        context = super().get_context_data(**kwargs)
+        
+        search_term = self.request.GET.get('search', '')
+        context['query'] = search_term
+        
+        context['count'] = self.get_queryset().count()
+        
+        return context
