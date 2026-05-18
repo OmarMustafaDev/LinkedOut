6b2ba34eb70361d3aa8790c87852de3cba1bb15d
AhmedJamea <ahmedgamaabc@gmail.com>
2026-05-18 18:13:04 +0300

    Update profile.py

--- users/views/profile.py
+++ users/views/profile.py
@@ -6,10 +6,10 @@ def profile_view(request):
     user = request.user
     
     if request.method == 'POST':
-        # Grab the text from the textarea
+       
         new_description = request.POST.get('description')
         user.description = new_description
         user.save()
-        # You could add a success message here!
+        
         
     return render(request, 'users/profile.html', {'user': user})
